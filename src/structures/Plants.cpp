08b7f7e1101ddf3735ec8e0c6c0ae0b22b58515e
Alpha <alpha0010@users.noreply.github.com>
2025-04-02 23:44:45 -0400

    feat: add remaining trees

--- src/structures/Plants.cpp
+++ src/structures/Plants.cpp
@@ -5,7 +5,7 @@
 #include "ids/WallID.h"
 #include "structures/data/Trees.h"
 #include <iostream>
-#include <span>
+#include <set>
 
 bool isRegionEmpty(int x, int y, int width, int height, World &world)
 {
@@ -47,40 +47,8 @@ void growBamboo(int x, int y, Random &rnd, World &world)
 
 void growCactus(int x, int y, Random &rnd, World &world)
 {
-    if (x < 400 || x > world.getWidth() - 400 ||
-        world.getTile(x, y - 1).wallID != WallID::empty ||
-        rnd.getDouble(0, 1) > 0.07) {
-        return;
-    }
-    int waterCount = 0;
-    for (int i = -50; i < 50; ++i) {
-        for (int j = -25; j < 25; ++j) {
-            if (world.getTile(x + i, y + j).liquid == Liquid::water) {
-                ++waterCount;
-                if (waterCount > 5) {
-                    return;
-                }
-            }
-        }
-    }
-    int sandCount = 0;
-    for (int i = -10; i < 10; ++i) {
-        for (int j = -8; j < 8; ++j) {
-            switch (world.getTile(x + i, y + j).blockID) {
-            case TileID::cactusPlant:
-                return;
-            case TileID::sand:
-            case TileID::hardenedSand:
-            case TileID::ebonsand:
-            case TileID::hardenedEbonsand:
-            case TileID::crimsand:
-            case TileID::hardenedCrimsand:
-                ++sandCount;
-                break;
-            }
-        }
-    }
-    if (sandCount < 15 || !isRegionEmpty(x - 1, y - 8, 3, 8, world)) {
+    if (rnd.getDouble(0, 1) > 0.07 ||
+        !isRegionEmpty(x - 1, y - 8, 3, 8, world)) {
         return;
     }
     int cactusSize = rnd.getInt(3, 7);
@@ -103,11 +71,93 @@ void growCactus(int x, int y, Random &rnd, World &world)
     }
 }
 
+void growPalmTree(int x, int y, Random &rnd, World &world)
+{
+    if (rnd.getDouble(0, 1) > 0.3) {
+        return;
+    }
+    for (int i = 0; i < 3; ++i) {
+        if (world.getTile(x + i, y).blockID != TileID::sand) {
+            return;
+        }
+    }
+    if (world.getTile(x - 1, y - 2).blockID == TileID::palmTree ||
+        world.getTile(x - 2, y - 2).blockID == TileID::palmTree) {
+        return;
+    }
+    int height = rnd.getInt(10, 20);
+    if (!isRegionEmpty(x, y - 4 - height, 3, height + 4, world)) {
+        return;
+    }
+    int bend = 0;
+    int targetBend = 2 * rnd.getInt(-8, 8);
+    for (int j = 0; j < height; ++j) {
+        Tile &tile = world.getTile(x + 1, y - j - 1);
+        tile.blockID = TileID::palmTree;
+        tile.frameX = j == height - 1 ? 22 * rnd.getInt(4, 6)
+                      : j == 0        ? 66
+                                      : 22 * rnd.getInt(0, 2);
+        tile.frameY = bend;
+        if (j * 4 > height) {
+            if (bend > targetBend) {
+                bend -= 2;
+            } else if (bend < targetBend) {
+                bend += 2;
+            }
+        }
+    }
+}
+
+void growSandPlant(int x, int y, Random &rnd, World &world)
+{
+    Tile &probeTile = world.getTile(x, y - 1);
+    if (probeTile.wallID != WallID::empty || probeTile.liquid != Liquid::none) {
+        return;
+    }
+    int sandCount = 0;
+    for (int i = -10; i < 10; ++i) {
+        for (int j = -8; j < 8; ++j) {
+            switch (world.getTile(x + i, y + j).blockID) {
+            case TileID::cactusPlant:
+                return;
+            case TileID::sand:
+            case TileID::hardenedSand:
+            case TileID::ebonsand:
+            case TileID::hardenedEbonsand:
+            case TileID::crimsand:
+            case TileID::hardenedCrimsand:
+                ++sandCount;
+                break;
+            }
+        }
+    }
+    if (sandCount < 15) {
+        return;
+    }
+    if (x < 380 || x > world.getWidth() - 380) {
+        growPalmTree(x, y, rnd, world);
+        return;
+    }
+    int waterCount = 0;
+    for (int i = -50; i < 50; ++i) {
+        for (int j = -25; j < 25; ++j) {
+            if (world.getTile(x + i, y + j).liquid == Liquid::water) {
+                ++waterCount;
+                if (waterCount > 5) {
+                    growPalmTree(x, y, rnd, world);
+                    return;
+                }
+            }
+        }
+    }
+    growCactus(x, y, rnd, world);
+}
+
 void growTree(
     int x,
     int y,
     int groundTile,
-    std::span<const Data::Tree> templateTrees,
+    int treeTile,
     Random &rnd,
     World &world)
 {
@@ -116,20 +166,39 @@ void growTree(
             return;
         }
     }
+    std::set<int> treeTiles{
+        TileID::tree,
+        TileID::sakuraTree,
+        TileID::yellowWillowTree,
+        TileID::ashTree,
+        TileID::amethystTree,
+        TileID::topazTree,
+        TileID::sapphireTree,
+        TileID::emeraldTree,
+        TileID::rubyTree,
+        TileID::amberTree,
+        TileID::diamondTree};
+    if (treeTiles.contains(world.getTile(x - 1, y - 2).blockID) ||
+        treeTiles.contains(world.getTile(x - 2, y - 2).blockID)) {
+        return;
+    }
     int height = rnd.getInt(7, 15);
     if (!isRegionEmpty(x, y - 4 - height, 3, height + 4, world)) {
         return;
     }
     for (int j = 0; j < height; ++j) {
         TileBuffer tree =
-            Data::getTree(rnd.select(templateTrees), world.getFramedTiles());
-        int treeRow = j == 0            ? 0
-                      : j == height - 1 ? tree.getHeight() - 1
-                                        : rnd.getInt(1, tree.getHeight() - 2);
+            Data::getTree(rnd.select(Data::trees), world.getFramedTiles());
+        int treeRow = j > height - 3 ? j - height + tree.getHeight()
+                      : j < 2        ? j
+                                     : rnd.getInt(2, tree.getHeight() - 3);
         for (int i = 0; i < tree.getWidth(); ++i) {
             Tile &tile = world.getTile(x + i, y + j - height);
             int curWall = tile.wallID;
             tile = tree.getTile(i, treeRow);
+            if (tile.blockID != TileID::empty) {
+                tile.blockID = treeTile;
+            }
             tile.wallID = curWall;
         }
     }
@@ -140,7 +209,8 @@ void genPlants(const LocationBins &locations, Random &rnd, World &world)
     std::cout << "Growing plants\n";
     for (const auto &bin : locations) {
         for (auto [x, y] : bin.second) {
-            switch (world.getTile(x, y).blockID) {
+            int curTileID = world.getTile(x, y).blockID;
+            switch (curTileID) {
             case TileID::ashGrass:
                 if (world.getTile(x, y - 1).liquid == Liquid::none &&
                     rnd.getDouble(0, 1) < 0.18) {
@@ -148,20 +218,30 @@ void genPlants(const LocationBins &locations, Random &rnd, World &world)
                         x,
                         y,
                         TileID::ashGrass,
-                        Data::ashTrees,
+                        TileID::ashTree,
                         rnd,
                         world);
                 }
                 break;
+            case TileID::corruptGrass:
+            case TileID::corruptJungleGrass:
+            case TileID::crimsonGrass:
+            case TileID::crimsonJungleGrass:
             case TileID::grass:
-                if (world.getTile(x, y - 1).liquid == Liquid::none &&
+            case TileID::snow:
+                if (y < world.getUndergroundLevel() &&
+                    world.getTile(x, y - 1).liquid == Liquid::none &&
                     world.getTile(x, y - 1).wallID == WallID::empty &&
                     rnd.getDouble(0, 1) < 0.21) {
                     growTree(
                         x,
                         y,
-                        TileID::grass,
-                        Data::forestTrees,
+                        curTileID,
+                        curTileID == TileID::grass && rnd.getDouble(0, 1) < 0.1
+                            ? rnd.select(
+                                  {TileID::sakuraTree,
+                                   TileID::yellowWillowTree})
+                            : TileID::tree,
                         rnd,
                         world);
                 }
@@ -181,7 +261,7 @@ void genPlants(const LocationBins &locations, Random &rnd, World &world)
                             x,
                             y,
                             TileID::jungleGrass,
-                            Data::mahoganyTrees,
+                            TileID::tree,
                             rnd,
                             world);
                     }
@@ -192,7 +272,7 @@ void genPlants(const LocationBins &locations, Random &rnd, World &world)
                         x,
                         y,
                         TileID::jungleGrass,
-                        Data::mahoganyUndergroundTrees,
+                        TileID::tree,
                         rnd,
                         world);
                 }
@@ -204,14 +284,36 @@ void genPlants(const LocationBins &locations, Random &rnd, World &world)
                         x,
                         y,
                         TileID::mushroomGrass,
-                        Data::mushroomTrees,
+                        TileID::tree,
                         rnd,
                         world);
                 }
                 break;
             case TileID::sand:
                 if (y < world.getUndergroundLevel()) {
-                    growCactus(x, y, rnd, world);
+                    growSandPlant(x, y, rnd, world);
+                }
+                break;
+            case TileID::stone:
+                if (y > world.getCavernLevel() &&
+                    world.getTile(x, y - 1).liquid == Liquid::none &&
+                    static_cast<int>(99999 * (1 + rnd.getFineNoise(x, y))) %
+                            100 ==
+                        0) {
+                    growTree(
+                        x,
+                        y,
+                        TileID::stone,
+                        rnd.select(
+                            {TileID::amethystTree,
+                             TileID::topazTree,
+                             TileID::sapphireTree,
+                             TileID::emeraldTree,
+                             TileID::rubyTree,
+                             TileID::amberTree,
+                             TileID::diamondTree}),
+                        rnd,
+                        world);
                 }
                 break;
             }
