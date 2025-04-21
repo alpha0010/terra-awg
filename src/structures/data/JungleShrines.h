c3067a908698dd52c5285a0b44e0a64ac4ebe1c1
Alpha <alpha0010@users.noreply.github.com>
2025-04-20 23:51:52 -0400

    feat(jungle): increase shrine variety

--- src/structures/data/JungleShrines.h
+++ src/structures/data/JungleShrines.h
@@ -7,7 +7,7 @@
 namespace Data
 {
 
-inline auto shrines = std::views::iota(0, 32);
+inline auto shrines = std::views::iota(0, 58);
 
 TileBuffer getShrine(int shrineId, const std::vector<bool> &framedTiles);
 
