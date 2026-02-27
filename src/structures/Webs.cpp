6a492ba8ba65542ec266bc51f82e167ecc38dcab
Alpha <alpha0010@users.noreply.github.com>
2026-02-26 21:21:37 -0500

    feat(cobwebs): improve spread at high conf values

--- src/structures/Webs.cpp
+++ src/structures/Webs.cpp
@@ -11,12 +11,13 @@
 
 void genWebs(Random &rnd, World &world)
 {
-    double patchThreshold = computeOreThreshold(1.5 * world.conf.cobwebs);
-    if (patchThreshold < -1.002) {
+    double mainThreshold = computeOreThreshold(1.5 * world.conf.cobwebs);
+    if (mainThreshold < -1.002) {
         return;
     }
     std::cout << "Spreading webs\n";
     rnd.shuffleNoise();
+    double secondaryThreshold = computeOreThreshold(0.08 * world.conf.cobwebs);
     constexpr auto targetWalls = frozen::make_set<int>(
         {WallID::Unsafe::dirt,
          WallID::Unsafe::ebonstone,
@@ -58,13 +59,21 @@ void genWebs(Random &rnd, World &world)
          WallID::Unsafe::fracturedStone});
     parallelFor(
         std::views::iota(0, world.getWidth()),
-        [patchThreshold, &targetWalls, &rnd, &world](int x) {
+        [mainThreshold, secondaryThreshold, &targetWalls, &rnd, &world](int x) {
             for (int y = world.getSurfaceLevel(x); y < world.getHeight(); ++y) {
                 Tile &tile = world.getTile(x, y);
                 if (tile.blockID != TileID::empty ||
                     tile.liquid != Liquid::none ||
-                    !targetWalls.contains(tile.wallID)) {
+                    tile.wallID == WallID::empty) {
                     continue;
+                }
+                double patchThreshold = mainThreshold;
+                if (!targetWalls.contains(tile.wallID)) {
+                    if (secondaryThreshold > -0.8) {
+                        patchThreshold = secondaryThreshold;
+                    } else {
+                        continue;
+                    }
                 }
                 double threshold =
                     1.5 - 2 * (rnd.getCoarseNoise(2 * x, 2 * y) + 1.1) /
